43e5a062d70c6356f2d4bbeb8ab30e0df4ae0942
Andy Lin <andyzanli@gmail.com>
2026-06-27 08:45:10 -0700

    fix errors

--- src/server/routes/triggers.ts
+++ src/server/routes/triggers.ts
@@ -11,7 +11,7 @@ triggers.post('/on-app-install', async (c) => {
     return c.json<TriggerResponse>(
       {
         status: 'success',
-        message: `Post created in subreddit ${context.subredditName} with id ${post.id} (trigger: ${input.type})`,
+        message: `Post created in subreddit ${context.subredditName} (trigger: ${input.type})`,
       },
       200
     );
