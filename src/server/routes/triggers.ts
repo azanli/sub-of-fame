be32822636c5dbead47f771f3dc46af2f8d7d4d2
Andy Lin <andyzanli@gmail.com>
2026-06-26 23:53:32 -0700

    remove more example code

--- src/server/routes/triggers.ts
+++ src/server/routes/triggers.ts
@@ -1,13 +1,11 @@
 import { Hono } from 'hono';
 import type { OnAppInstallRequest, TriggerResponse } from '@devvit/web/shared';
 import { context } from '@devvit/web/server';
-import { createPost } from '../core/post';
 
 export const triggers = new Hono();
 
 triggers.post('/on-app-install', async (c) => {
   try {
-    const post = await createPost();
     const input = await c.req.json<OnAppInstallRequest>();
 
     return c.json<TriggerResponse>(
