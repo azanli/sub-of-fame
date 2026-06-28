f0f1c572daaada5be52a8df63589b728efcf7b4c
Andy Lin <andyzanli@gmail.com>
2026-06-27 22:50:34 -0700

    slim down curated options and fix images

--- src/shared/subreddits.ts
+++ src/shared/subreddits.ts
@@ -8,51 +8,19 @@ export const CURATED_SUBREDDITS: SubredditOption[] = [
   {
     name: 'askreddit',
     displayName: 'AskReddit',
-    iconUrl: 'https://styles.redditmedia.com/t5_2qh1i/styles/communityIcon_9oaqfn1y7p01.png',
+    iconUrl:
+      'https://styles.redditmedia.com/t5_2qh1i/styles/communityIcon_p6kb2m6b185b1.png?width=128&frame=1&auto=webp&s=1124511c3a95cc4ec094a4e8886de1e08a0f1e0a',
   },
   {
-    name: 'gaming',
-    displayName: 'Gaming',
-    iconUrl: 'https://styles.redditmedia.com/t5_2qh03/styles/communityIcon_4g1uo0oh7k571.png',
-  },
-  {
-    name: 'worldnews',
-    displayName: 'World News',
-    iconUrl: 'https://styles.redditmedia.com/t5_2qh13/styles/communityIcon_m92ya1hbznt91.png',
-  },
-  {
-    name: 'todayilearned',
-    displayName: 'Today I Learned',
-    iconUrl: 'https://styles.redditmedia.com/t5_2qqjc/styles/communityIcon_p8kfxzqpejb71.png',
-  },
-  {
-    name: 'science',
-    displayName: 'Science',
-    iconUrl: 'https://styles.redditmedia.com/t5_mouw/styles/communityIcon_itx4gctyex271.png',
-  },
-  {
-    name: 'movies',
-    displayName: 'Movies',
-    iconUrl: 'https://styles.redditmedia.com/t5_2qh3s/styles/communityIcon_jo2z5g3a5sz41.png',
-  },
-  {
-    name: 'technology',
-    displayName: 'Technology',
-    iconUrl: 'https://styles.redditmedia.com/t5_2qh16/styles/communityIcon_ikg3h2h2u3j71.png',
+    name: 'cats',
+    displayName: 'cats',
+    iconUrl:
+      'https://styles.redditmedia.com/t5_2qhta/styles/communityIcon_2fsd7ji8awg91.png?width=128&frame=1&auto=webp&s=e6227ad9f13cfeed4046201f51686e92bbb37d68',
   },
   {
     name: 'sports',
-    displayName: 'Sports',
-    iconUrl: 'https://styles.redditmedia.com/t5_2qh6c/styles/communityIcon_6wcm7l1wccl81.png',
-  },
-  {
-    name: 'music',
-    displayName: 'Music',
-    iconUrl: 'https://styles.redditmedia.com/t5_2qh1s/styles/communityIcon_t0bkjeqplqr61.png',
-  },
-  {
-    name: 'television',
-    displayName: 'Television',
-    iconUrl: 'https://styles.redditmedia.com/t5_2qh6e/styles/communityIcon_o14hzxr6vn861.png',
+    displayName: 'sports',
+    iconUrl:
+      'https://styles.redditmedia.com/t5_2qgzy/styles/communityIcon_rvt3zjh1fc551.png?width=128&frame=1&auto=webp&s=3947f8eb6f99d7f6869637dc0d8cabc904495259',
   },
 ];
