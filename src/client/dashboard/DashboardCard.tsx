4378220f936be2181721265d1f8b59a698de26bb
Andy Lin <andyzanli@gmail.com>
2026-06-28 21:13:43 -0700

    update border animation

--- src/client/dashboard/DashboardCard.tsx
+++ src/client/dashboard/DashboardCard.tsx
@@ -88,15 +88,26 @@ export const DashboardCard = (props: DashboardCardProps) => {
 
   if (isLoading) {
     return (
-      <div className="relative overflow-hidden rounded-xl p-[2px]">
+      <div className="relative rounded-xl">
+        {/* 1. The Spinning Border Layer (Masked to only show the edges) */}
         <div
           aria-hidden
-          className="absolute inset-[-100%] animate-spin bg-[conic-gradient(from_0deg,transparent_0deg,transparent_250deg,#fb923c_285deg,#d93900_360deg)]"
-        />
+          className="pointer-events-none absolute inset-0 overflow-hidden rounded-xl p-[2px]"
+          style={{
+            WebkitMask:
+              'linear-gradient(#fff 0 0) content-box, linear-gradient(#fff 0 0)',
+            WebkitMaskComposite: 'xor',
+            maskComposite: 'exclude',
+          }}
+        >
+          <div className="absolute inset-[-100%] animate-spin bg-[conic-gradient(from_0deg,transparent_0deg,transparent_250deg,#fb923c_285deg,#d93900_360deg)]" />
+        </div>
+
+        {/* 2. The Card Content (Now free to use bg-transparent) */}
         <button
           type="button"
           disabled
-          className={`${cardButtonClasses} ${loadingCardInnerClasses}`}
+          className={`${cardButtonClasses} relative z-10 rounded-xl border-0 bg-transparent`}
         >
           {cardContent}
         </button>
