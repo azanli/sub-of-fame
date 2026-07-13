be7158e636f8b18e1e24ab576794820b121a778d
Andy Lin <andyzanli@gmail.com>
2026-07-13 16:42:43 -0700

    big dipper constellation

--- src/client/dashboard/DashboardSpaceScene.tsx
+++ src/client/dashboard/DashboardSpaceScene.tsx
@@ -42,51 +42,84 @@ export const DashboardSpaceScene = ({
             </div>
           </div>
         )}
-        <svg
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-          fill="white"
-          className="absolute top-10 left-1/4 twinkle-star"
-          style={{ animationDelay: '0.8s', animationDuration: '8s' }}
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path d="M12 2C12.3 8.3 12.3 8.3 19 12C12.3 15.7 12.3 15.7 12 22C11.7 15.7 11.7 15.7 5 12C11.7 8.3 11.7 8.3 12 2Z" />
-        </svg>
-        <svg
-          width="18"
-          height="18"
-          viewBox="0 0 24 24"
-          fill="white"
-          className="absolute bottom-12 left-1/3 twinkle-star"
-          style={{ animationDelay: '1.2s', animationDuration: '2s' }}
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path d="M12 2C12.3 8.3 12.3 8.3 19 12C12.3 15.7 12.3 15.7 12 22C11.7 15.7 11.7 15.7 5 12C11.7 8.3 11.7 8.3 12 2Z" />
-        </svg>
-        <svg
-          width="16"
-          height="16"
-          viewBox="0 0 24 24"
-          fill="white"
-          className="absolute top-1/2 right-28 twinkle-star"
-          style={{ animationDelay: '0.6s', animationDuration: '2.5s' }}
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path d="M12 2C12.3 8.3 12.3 8.3 19 12C12.3 15.7 12.3 15.7 12 22C11.7 15.7 11.7 15.7 5 12C11.7 8.3 11.7 8.3 12 2Z" />
-        </svg>
-        <svg
-          width="12"
-          height="12"
-          viewBox="0 0 24 24"
-          fill="white"
-          className="absolute top-4 right-6 twinkle-star"
-          style={{ animationDelay: '2.4s', animationDuration: '3.5s' }}
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path d="M12 2C12.3 8.3 12.3 8.3 19 12C12.3 15.7 12.3 15.7 12 22C11.7 15.7 11.7 15.7 5 12C11.7 8.3 11.7 8.3 12 2Z" />
-        </svg>
       </div>
+      <svg
+        width="20"
+        height="20"
+        viewBox="0 0 24 24"
+        fill="white"
+        className="absolute top-[20%] left-[25%] twinkle-star"
+        style={{ animationDelay: '0.8s', animationDuration: '8s' }}
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path d="M12 2C12.3 8.3 12.3 8.3 19 12C12.3 15.7 12.3 15.7 12 22C11.7 15.7 11.7 15.7 5 12C11.7 8.3 11.7 8.3 12 2Z" />
+      </svg>
+      <svg
+        width="19"
+        height="19"
+        viewBox="0 0 24 24"
+        fill="white"
+        className="absolute top-[5%] left-[40%] twinkle-star"
+        style={{ animationDelay: '0.4s', animationDuration: '6s' }}
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path d="M12 2C12.3 8.3 12.3 8.3 19 12C12.3 15.7 12.3 15.7 12 22C11.7 15.7 11.7 15.7 5 12C11.7 8.3 11.7 8.3 12 2Z" />
+      </svg>
+      <svg
+        width="18"
+        height="18"
+        viewBox="0 0 24 24"
+        fill="white"
+        className="absolute top-[35%] left-[53%] twinkle-star"
+        style={{ animationDelay: '1.2s', animationDuration: '12s' }}
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path d="M12 2C12.3 8.3 12.3 8.3 19 12C12.3 15.7 12.3 15.7 12 22C11.7 15.7 11.7 15.7 5 12C11.7 8.3 11.7 8.3 12 2Z" />
+      </svg>
+      <svg
+        width="10"
+        height="10"
+        viewBox="0 0 24 24"
+        fill="white"
+        className="absolute top-[50%] left-[65%] twinkle-star"
+        style={{ animationDelay: '0.4s', animationDuration: '3s' }}
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path d="M12 2C12.3 8.3 12.3 8.3 19 12C12.3 15.7 12.3 15.7 12 22C11.7 15.7 11.7 15.7 5 12C11.7 8.3 11.7 8.3 12 2Z" />
+      </svg>
+      <svg
+        width="18"
+        height="18"
+        viewBox="0 0 24 24"
+        fill="white"
+        className="absolute top-[80%] left-[67%] twinkle-star"
+        style={{ animationDelay: '1.2s', animationDuration: '2s' }}
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path d="M12 2C12.3 8.3 12.3 8.3 19 12C12.3 15.7 12.3 15.7 12 22C11.7 15.7 11.7 15.7 5 12C11.7 8.3 11.7 8.3 12 2Z" />
+      </svg>
+      <svg
+        width="14"
+        height="14"
+        viewBox="0 0 24 24"
+        fill="white"
+        className="absolute top-[85%] left-[85%] twinkle-star"
+        style={{ animationDelay: '0.6s', animationDuration: '2.5s' }}
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path d="M12 2C12.3 8.3 12.3 8.3 19 12C12.3 15.7 12.3 15.7 12 22C11.7 15.7 11.7 15.7 5 12C11.7 8.3 11.7 8.3 12 2Z" />
+      </svg>
+      <svg
+        width="18"
+        height="18"
+        viewBox="0 0 24 24"
+        fill="white"
+        className="absolute top-[45%] left-[87%] twinkle-star"
+        style={{ animationDelay: '2.4s', animationDuration: '3.5s' }}
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path d="M12 2C12.3 8.3 12.3 8.3 19 12C12.3 15.7 12.3 15.7 12 22C11.7 15.7 11.7 15.7 5 12C11.7 8.3 11.7 8.3 12 2Z" />
+      </svg>
     </div>
   );
 };
