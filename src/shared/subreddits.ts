654fa949570067a68f34eed2c25d1f08c2386d6a
Andy Lin <andyzanli@gmail.com>
2026-07-13 22:47:04 -0700

    remove log

--- src/shared/subreddits.ts
+++ src/shared/subreddits.ts
@@ -8,7 +8,6 @@ export const normalizeSubredditDisplayName = (value: string): string =>
   value.trim().replace(/^r\//i, '').split(' ')[0] ?? '';
 
 export const formatSubredditLabel = (displayName: string): string => {
-  console.log('displayName', displayName);
   if (displayName === 'Global Leaderboard') {
     return 'Global Leaderboard';
   }
